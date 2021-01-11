633e521c0607a5168948396cbd268ca29bcc303f
mdraw <martin.drawitsch@gmail.com>
2021-01-11 03:54:12 +0100

    Make scraping a lot faster, write results directly
    - Scraping now uses multiple threads or optionally multiple processes
      to download, parse and store website contents
    - Articles that were sucessfully downloaded are now immediately written
      to disk. You no longer have to wait hours to get the results.
    - Articles are no longer saved in one huge collection JSON.
      Instead they are saved to separate files in a common parent directory.
      The file names are based on the article URLs so each article can still
      be uniquely identified based on its file name.
    - Options for site downloads are now better optimized for speed

--- scraping/collect_articles.py
+++ scraping/collect_articles.py
@@ -5,33 +5,31 @@ Script for scraping news articles from websites.
 
 
 Example usage for downloading all articles listed in a csv and storing them in the
-file articles.json:
+directory /tmp/news-articles:
 
-    $ python3 collect_articles.py urls.txt -o articles.json
+    $ python3 collect_articles.py urls.txt /tmp/news-articles
 
 
-Scraped articles are stored in a JSON file with the following structure:
+Scraped articles are stored in JSON files with the following structure:
 
 {
-    "<ARTICLE_URL>": {
-        "text": "<ARTICLE-TEXT>",
-        "title": "<ARTICLE_TITLE>",
-        ...
-    },
+    "text": "<ARTICLE-TEXT>",
+    "title": "<ARTICLE_TITLE>",
+    "date": "<ARTICLE_DATE>",
+    "url": "<ARTICLE_URL">
     ...
 }
 
-At the top level, each article is uniquely identified with its URL
-(<ARTICLE_URL>) and under each URL key you can find the contents ("text"),
-title and additional site-dependent metadata.
-
-
+Each article is saved to its own file in the specified directory.
+File names are automatically chosen based on the respective article URLs.
+In each JSON file you can find the contents ("text"), title, publication date,
+URL and additional site-dependent metadata.
 """
 
 import argparse
 import logging
 
-from scraping import read_sources, scrape_articles, write_to_json, change_log_file_path
+from scraping import read_sources, scrape_and_store_articles, change_log_file_path
 
 logger = logging.getLogger('scraping')
 
@@ -45,18 +43,30 @@ def main():
     )
     parser.add_argument(
         'output_path',
-        help='Path to JSON file in which articles should be stored.',
+        help='Path to the directory in which the articles should be stored.',
     )
     parser.add_argument(
         '--log-path',
         help='Path to log file (default: /tmp/scraping.log)',
         default='/tmp/scraping.log'
     )
+    parser.add_argument(
+        '-m', '--enable-mp',
+        help='Enable multiprocessing (faster, but logging may be broken)',
+        action='store_true'
+    )
+    parser.add_argument(
+        '-n', '--num-workers',
+        help='Number of concurrent worker processes/threads',
+        type=int
+    )
     args = parser.parse_args()
 
     source_path = args.source_path
     output_path = args.output_path
     log_path = args.log_path
+    enable_mp = args.enable_mp
+    num_workers = args.num_workers
 
     # Set up logging: Override log file path
     change_log_file_path(log_path)
@@ -66,9 +76,12 @@ def main():
     article_sources = read_sources(source_path)
     if len(article_sources) == 0:
         raise RuntimeError(f'No articles found. Aborting...')
-    scraped_articles = scrape_articles(article_sources)
-    # Store article collection as JSON
-    write_to_json(scraped_articles, output_path)
+    scrape_and_store_articles(
+        article_sources,
+        parent_dir=output_path,
+        multiprocessing=enable_mp,
+        num_workers=num_workers
+    )
 
 if __name__ == '__main__':
     main()
--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,15 +1,25 @@
 """Library for scraping news articles from websites, based on newspaper"""
 
 import csv
+import functools
 import json
 import logging
 import os
 
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from dataclasses import dataclass
-from typing import Dict, Sequence
+from typing import Any, Dict, Optional, Sequence
 
 import newspaper
 from tqdm import tqdm
+
+
+NP_DEFAULT_CONFIG = {
+    'language': 'de',
+    'fetch_images': False,
+    'memoize_articles': False,
+    'request_timeout': 7,
+}
 
 
 def setup_logging(log_path: str = '/tmp/scraping.log') -> logging.Logger:
@@ -33,7 +43,6 @@ def setup_logging(log_path: str = '/tmp/scraping.log') -> logging.Logger:
 
 # Instantiate module logger
 logger = setup_logging()
-
 
 
 def change_log_file_path(new_log_path):
@@ -69,21 +78,52 @@ def read_sources(path: str) -> Sequence[ArticleSource]:
             article_sources.append(ArticleSource(url=url, date=date))
     return article_sources
 
-def load_article(source: ArticleSource) -> Dict[str, str]:
-    """Download and parse article, fill metadata, store date from the given article source"""
-    article = newspaper.Article(source.url)
+
+def load_article(
+        source: ArticleSource,
+        np_config: Optional[Dict[str, Any]] = None
+) -> Dict[str, str]:
+    """Download and parse article, fill metadata,
+    store date from the given article source."""
+    np_config = NP_DEFAULT_CONFIG if np_config is None else np_config
+    np_article = newspaper.Article(source.url, **np_config)
     try:
-        article.download()
-        article.parse()
+        np_article.download()
+        np_article.parse()
     except newspaper.ArticleException:
-        logger.debug(f'Could not download {article.url}')
+        logger.debug(f'Could not download {np_article.url}')
         return None
-    # Store metadata and article text in the dict
-    entry = dict(article.meta_data)
-    entry['text'] = article.text
-    entry['title'] = article.title
-    entry['date'] = source.date
-    return entry
+    # Initialize article dict with misc. metadata from newspaper (site-dependent!)
+    article = dict(np_article.meta_data)
+    # Manually ensure text, title, date and url are properly registered
+    article['text'] = np_article.text
+    article['title'] = np_article.title
+    article['date'] = source.date
+    article['url'] = source.url
+    return article
+
+
+def filename_from_url(url: str, ext: str = '.json') -> str:
+    # Only keep alphanumeric characters because they are definitely safe
+    sanitized_url = ''.join(c for c in url if c.isalnum())
+    filename = f'{sanitized_url}{ext}'
+    return filename
+
+
+def load_and_store_article(
+        source: ArticleSource,
+        parent_dir: str,
+        np_config: Optional[Dict[str, Any]] = None
+) -> None:
+    filename = filename_from_url(source.url)
+    full_path = f'{parent_dir}/{filename}'
+    if os.path.exists(full_path):
+        logger.debug(f'{full_path} already exists, skipping...')
+        return
+
+    article = load_article(source=source, np_config=np_config)
+    if article is not None:
+        write_to_json(article, full_path)
 
 
 def scrape_articles(sources: Sequence[ArticleSource]) -> Sequence[Dict[str, str]]:
@@ -91,14 +131,42 @@ def scrape_articles(sources: Sequence[ArticleSource]) -> Sequence[Dict[str, str]
     scraped_articles = {}
     logger.info('Downloading and parsing articles...')
     for article_source in tqdm(sources):
-        entry = load_article(article_source)
-        if entry is not None:
-            scraped_articles[article_source.url] = entry
+        article = load_article(article_source)
+        if article is not None:
+            scraped_articles[article_source.url] = article
     return scraped_articles
+
+
+def scrape_and_store_articles(
+        sources: Sequence[ArticleSource],
+        parent_dir: str,
+        multiprocessing: bool = True,
+        num_workers: int = 32
+) -> None:
+    """Scrape articles from given article sources and immediately store them on disk.
+    
+    Uses multiprocessing/multithreading."""
+    parent_dir = os.path.expanduser(parent_dir)
+    # Partially applied function to always store to the same parent dir
+    _load_and_store_article = functools.partial(
+        load_and_store_article,
+        parent_dir=parent_dir
+    )
+    os.makedirs(parent_dir, exist_ok=True)
+    logger.info(f'Downloading and parsing articles, storing in {parent_dir} ...')
+    # TODO: Make logging multi-processing-safe
+    Executor = ProcessPoolExecutor if multiprocessing else ThreadPoolExecutor
+    with Executor(max_workers=num_workers) as executor:
+        # Wrapping in list(tqdm(...)) necessary to enable progress bar
+        list(tqdm(
+            executor.map(_load_and_store_article, sources),
+            total=len(sources)
+        ))
+
 
 def write_to_json(articles: Dict[str, str], json_path: str) -> None:
     """Write article collection dict to a JSON file"""
-    logger.info(f'Storing collection in {json_path}')
+    # logger.debug(f'Storing collection in {json_path}')
     json_path = os.path.expanduser(json_path)
-    with open(json_path, 'w') as f:
-        json.dump(articles, f)
+    with open(json_path, 'w', encoding='utf-8') as f:
+        json.dump(articles, f, ensure_ascii=False)
