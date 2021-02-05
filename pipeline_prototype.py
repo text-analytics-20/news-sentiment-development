8d0822e316a1c65bb8a0cd8734b2001c9a4269ed
silotin <simon.luedke@gmx.de>
2021-02-05 11:47:02 +0100

    control pipeline from config.ini, run w2v with different selections

--- pipeline_prototype.py
+++ pipeline_prototype.py
@@ -1,9 +1,13 @@
-import glob 
+import configparser
+import glob
+from sentiment_analysis.word2vec_sentiment import *
 #from sentiment_analysis.sentimentDictionary import sentimentDictionary
 #from sentiment_analysis.fuctionality_sentiment_analysis import analyse_sentiment, listSearchTerm
 import article_selection.article_selection as artSel
 import json
+import sys
 import os
+from time import time
 
 def calulate_sentiment(content, save_path, article_path):
     try: 
@@ -17,54 +21,79 @@ def calulate_sentiment(content, save_path, article_path):
         with open(save_path, "a") as dataFile:
             dataFile.write(data)
 
-def write_relevant_content_to_file(file_list, relevant_articles_file, new=False):
+def write_relevant_content_to_file(file_list, relevant_articles_file, new=False, output_after=5000):
     if new:
         try:
             os.remove(relevant_articles_file)
         except FileNotFoundError:
             print("file does already not exist")
-    for json_file in jason_file_list:
+    t=time()
+    print(f"Start selecting files. Number of files: {len(file_list)}")
+    counter=0
+    new_cont={}
+    for json_file in file_list:
+        if counter%output_after==0 and counter>0:
+            print(f"{counter} of the files is read. {round(float(counter)/len(file_list)*100 , 0)}% Time since start: {round((time() - t) / 60, 2)} min")
+            print(f"Approximate time till end is: {round((len(file_list)/float(counter) * (time() - t)-(time() - t)) / 60 , 2)} min") 
+        counter+=1
         with open(json_file ,"r") as jf:
             content=json.load(jf)
             if(artSel.is_topic_relevant(content)):
-                new_cont = {json_file : content}
-                try:    
-                    with open(relevant_articles_file ,"r+") as ra:
-                        content_ra = json.load(ra)
-                        content_ra.update(new_cont)
-                        ra.seek(0)
-                        json.dump(content_ra,ra)
-                except FileNotFoundError:
-                    with open(relevant_articles_file ,"w") as raf:
-                        json.dump(new_cont,raf)
+                new_cont[json_file]=content
+    try:    
+        with open(relevant_articles_file ,"r+") as ra:
+            content_ra = json.load(ra)
+            content_ra.update(new_cont)
+            ra.seek(0)
+            json.dump(content_ra,ra)
+    except FileNotFoundError:
+        # happens if new is enabled or function called the first time for a filepath
+        with open(relevant_articles_file ,"w") as raf:
+            json.dump(new_cont,raf)
+    print(f"All files is read. Time since start: {round((time() - t) / 60, 2)}")
 
 
 if __name__ == "__main__":
-    data_path_list = ["data/ta_scrape100k_2007/","data/ta_scrape100k_2008/","data/ta_scrape100k_2009/"]
-    relevant_articles = "data/relevant_articles.json"
-    save_path = "data/sentiment.csv"
-    if True:
+
+    config_file = sys.argv[1]
+    config = configparser.ConfigParser()		
+    config.read(config_file) 
+
+    # ==============================
+    # Selection of relevant Articles
+    # ==============================
+    if config.getboolean("ArticleSelection","run_article_selection"):
+        #create input filepath for article selection
+        base_path=config.get("ArticleSelection","input_path_base")
+        start_year=config.getint("ArticleSelection","start_year")
+        end_year=config.getint("ArticleSelection","end_year")
+        data_path_list = [base_path+str(year)+"/" for year in range(start_year,end_year+1)]
+            
         # create list of all data paths
-        jason_file_list = []
+        json_file_list = []
         for path in data_path_list:
-            jason_file_list +=  [file_path for file_path in glob.glob(path+"*.json")]
+            json_file_list +=  [file_path for file_path in glob.glob(path+"*.json")]
         
         # open all files containing an article and if the topic is relevannt save it to a collectiv json file 
-        write_relevant_content_to_file(jason_file_list, relevant_articles,True)
+        write_relevant_content_to_file(json_file_list, config.get("ArticleSelection","output_file") ,True)
+    
+    # ===================
+    # Word2Vec analysis
+    # ===================
+    if config.getboolean("Analysis","run_w2v"): 
+        input_file = config.get("Analysis","input_file") 
+        search_words = config.get("Analysis","search_words").lower().split(",")
+        base_output_path=config.get("Analysis","output_base_w2v")
+        start_year = config.get("Analysis","start_year")
+        end_year = config.get("Analysis","end_year")
+        print(input_file)
+        if config.getboolean("Analysis","run_by_year"):
+            similarity_by_year(input_file, base_output_path ,search_words, start_year, end_year)
+        if config.getboolean("Analysis","run_by_publisher"):
+            similarity_by_publisher(input_file, base_output_path ,search_words, start_year, end_year)
+        if config.getboolean("Analysis","run_by_publisher_by_year"):
+            similarity_by_year_and_publisher(input_file, base_output_path ,search_words, start_year, end_year)
 
-    if True: 
-        with open(relevant_articles,"r") as f:
-            content = json.load(f)
-            i=0
-            for k in content:
-                if i>10:
-                    break 
-                i+=1
-                print(k["date"])
-    
-    
-    #calulate_sentiment(content, save_path, json_file)
-    #print(publishers)
 #TO DO:
 # want to check for doubles for this use same sentiDic entity for all articles   
 # this also gives the opportunity to use the saved sentences of all articles for creation of a word-cloud  
--- sentiment_analysis/word2vec_sentiment.py
+++ sentiment_analysis/word2vec_sentiment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 import spacy
 import regex as re
 from gensim.models.phrases import Phrases, Phraser
@@ -6,12 +7,11 @@ import multiprocessing
 from gensim.models import Word2Vec
 from time import time
 import logging  # Setting up the loggings to monitor gensim
-logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
+logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.WARN)
 
-def hash(astring):
-    return ord(astring[0])
 
 class SentiW2v:
+    text = None
     def __init__(self):
         self.w2v_model = Word2Vec(min_count=20,
                             window=2,
@@ -31,14 +31,21 @@ class SentiW2v:
         if len(txt)>2:
             return ' '.join(txt)
 
-    def train(self,path):
+    def set_text_from_file(self,path):
         df = pd.read_json(path, orient='index')        
-        df = df[["date","og","text"]]
-        # cleaning using regex
-        brief_cleaning = (re.sub("[^A-Za-züäöÜÄÖß']",' ', str(row)).lower() for row in df["text"])
-        
+        #self.df = self.df[["date","og","text"]]
+        self.text=df["text"]
+
+    def set_text_from_pandas(self, data):#
+        df = data
+        #self.df = df[["date","og","text"]]
+        self.text=df["text"]
+
+    def clean_and_train(self):
+        brief_cleaning = (re.sub("[^A-Za-züäöÜÄÖß']",' ', str(row)).lower() for row in self.text)
+        print(len(self.text))
         t=time()
-        txt= [self.cleaning(doc) for doc in self.nlp.pipe(brief_cleaning, batch_size=5000,n_threads=-1)]
+        txt= [self.cleaning(doc) for doc in self.nlp.pipe(brief_cleaning, batch_size=50,n_threads=-1)]
         print('Time to clean up everything: {} mins'.format(round((time() - t) / 60, 2)))
 
         df_clean = pd.DataFrame({'clean': txt})
@@ -51,38 +58,123 @@ class SentiW2v:
         bigram = Phraser(phrases)
         sentences = bigram[sent]
 
-        # most frequent words
-
-        word_freq = {}
-        for sent in sentences:
-            for i in sent:
-                if i in word_freq:
-                    word_freq[i]+=1
-                else:
-                    word_freq[i]=1
-        #print(len(word_freq))
-        #sorted(word_freq, key=word_freq.get, reverse=True)[:10]
         t = time()
         self.w2v_model.build_vocab(sentences, progress_per=10000)
         print('Time to build vocab: {} mins'.format(round((time() - t) / 60, 2)))
 
         t = time()
         self.w2v_model.train(sentences, total_examples=self.w2v_model.corpus_count, 
-                    epochs=30, report_delay=1)
+                                                            epochs=30, report_delay=1)
         print('Time to train the model: {} mins'.format(round((time() - t) / 60, 2)))
 
         self.w2v_model.init_sims(replace=True) # called when finished training for memory efficiency
 
     def print_most_similar(self, word):
         try:
-            print(sw.w2v_model.wv.most_similar(word))
+            print(self.w2v_model.wv.most_similar(word))
+        except KeyError:
+            print(f"The word {word} is not in the in vocabulary.")
+    def get_most_similar(self,word):
+        try:
+            return self.w2v_model.wv.most_similar(word)
         except KeyError:
             print(f"The word {word} is not in the in vocabulary.")
 
-sw=SentiW2v()
-sw.train("../data/relevant_articles.json")
 
-sw.print_most_similar("flüchtling")
-sw.print_most_similar("grenze")
-sw.print_most_similar("Abschiebung")
+def similarity_by_year(input_path :  str,output_path : str, search_words : list, start_year=2007, end_year=2015):
+    content=pd.read_json(input_path, orient="index")
+    content=content[["date","og","text","url"]]
+    most_sim={}
+    content["date"]=content['date'].astype('str')
+    for year in range(start_year,end_year+1):
+        print(f"\nStart analysis for year {year}")
+        sw=SentiW2v()
+        t=time()
+        sub_cont= content[content["date"].str.contains(str(year))]
+        print(f"Time to reduce content {round((time() - t) / 60, 2)} mins")
+        sw.set_text_from_pandas(sub_cont)
+        sw.clean_and_train()
+        most_sim[year] = { key : sw.get_most_similar(key) for key in search_words}
+    with open(output_path+"_by_year.json","w") as f:
+        json.dump(most_sim,f)
+
+def similarity_by_publisher(input_path : str,output_path : str, search_words : list, start_year=2007, end_year=2015):
+    content=pd.read_json(input_path, orient="index")
+    content=content[["date","og","text","url"]]
+    most_sim={}
+    list_publishers = []
+    for i , row in content.iterrows():
+        try:
+            publisher = row["url"].split("//")[1].split("/")[0].split(".")[1]
+        except KeyError:
+            print("ERROR")
+            print(row)
+            continue
+        if publisher in list_publishers:
+            continue
+        else:
+            list_publishers.append(publisher)
+            most_sim[publisher]=[]
+            sw=SentiW2v()
+            sub_cont=content[content["url"].str.contains(publisher)]
+            sw.set_text_from_pandas(sub_cont)
+            try:
+                sw.clean_and_train()
+                most_sim[publisher].append({ key : sw.get_most_similar(key) for key in search_words})            
+            except RuntimeError:
+                print("ERROR")
+    with open(output_path+"_by_publisher.json","w") as f:
+        json.dump(most_sim,f)
+
+def similarity_by_year_and_publisher(input_path : str, output_path : str,search_words : list, start_year=2007, end_year=2015):
+    content=pd.read_json(input_path, orient="index")
+    content=content[["date","og","text","url"]]
+    most_sim={}
+    list_publishers = []
+    for i , row in content.iterrows():
+        year = str(row["date"]) .split("-")[0]
+        try:
+            publisher = row["url"].split("//")[1].split("/")[0].split(".")[1]
+        except KeyError:
+            print("ERROR")
+            print(row)
+            continue
+        if publisher not in list_publishers:
+            list_publishers.append(publisher)
+            most_sim[publisher]={}
+
+        if publisher in list_publishers and year in most_sim[publisher]:
+            continue
+        sw=SentiW2v()
+        sub_cont=content[content["url"].astype(str).str.contains(publisher)] #content[content["url"].str.contains(publisher)]
+        sub_cont=sub_cont[sub_cont["date"].astype(str).str.contains(year)]
+        sw.set_text_from_pandas(sub_cont)
+        try:
+            sw.clean_and_train()
+            most_sim[publisher][year]={ key : sw.get_most_similar(key) for key in search_words}
+        except RuntimeError:
+            print("ERROR")
+            continue
+    with open(output_path+"_by_publisher_and_year.json","w") as f:
+        json.dump(most_sim,f)
+
+
+
+if __name__=="__main__": 
+    
+    # load content from json file to panda data frame and reduce it to the needed columns
+
+    input_file_path="../data/relevant_articles.json"
+    search_word_list=["flüchtling", "grenze", "ausländer", "abschiebung","migration", "einwanderung", "notunterkunft"]
+    
+    if False:
+        # selection by year
+        similarity_by_year(input_file_path,"hadsj.json",search_word_list)
         
+    if False:
+        # selection by publisher
+        similarity_by_publisher(input_file_path,"kljsdf.json",search_word_list)
+    
+    if True:
+        # selection by publisher
+        similarity_by_year_and_publisher(input_file_path,"hadsj.json",search_word_list)
