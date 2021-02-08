75718d705fb0a40316bd430e0772e199848bc3c2
mdraw <martin.drawitsch@gmail.com>
2021-02-08 05:01:56 +0100

    Add BERT-based sentiment analysis prototype
    Based on
    https://huggingface.co/oliverguhr/german-sentiment-bert
    and
    https://github.com/oliverguhr/german-sentiment-lib/blob/4e51588/germansentiment/sentimentmodel.py
    
    Adapted to use polarity scores instead of discrete argmax
    classification.

---
+++ sentiment_analysis/nn.py
@@ -0,0 +1,79 @@
+"""German news entiment classification using a pretrained BERT model.
+
+SentimentModel class based on https://huggingface.co/oliverguhr/german-sentiment-bert,
+    (C) 2020 Oliver Guhr (MIT License)"""
+
+import re
+
+from typing import List
+
+import torch
+import torch.nn.functional as F
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+
+
+# 0: pos, 1: neg, 2: neutral
+
+
+
+class GSBertPolarityModel():
+    """Code adapted from
+    https://github.com/oliverguhr/german-sentiment-lib/blob/4e5158/germansentiment/sentimentmodel.py"""
+
+    def __init__(self, model_name: str = "oliverguhr/german-sentiment-bert"):
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        self.clean_chars = re.compile(r'[^A-Za-züöäÖÜÄß ]', re.MULTILINE)
+        self.clean_http_urls = re.compile(r'https*\S+', re.MULTILINE)
+        self.clean_at_mentions = re.compile(r'@\S+', re.MULTILINE)
+
+    def replace_numbers(self,text: str) -> str:
+        return text.replace("0"," null").replace("1"," eins").replace("2"," zwei")\
+            .replace("3"," drei").replace("4"," vier").replace("5"," fünf") \
+            .replace("6"," sechs").replace("7"," sieben").replace("8"," acht") \
+            .replace("9"," neun")         
+
+    def clean_text(self,text: str)-> str:    
+        text = text.replace("\n", " ")        
+        text = self.clean_http_urls.sub('',text)
+        text = self.clean_at_mentions.sub('',text)        
+        text = self.replace_numbers(text)                
+        text = self.clean_chars.sub('', text) # use only text chars                          
+        text = ' '.join(text.split()) # substitute multiple whitespace with single whitespace   
+        text = text.strip().lower()
+        return text
+
+    @staticmethod
+    def probs2polarities(pnn):
+        """Transform softmax probs of a [positive, negative, neutral] classifier
+        into scalar polarity scores of range [-1, 1].
+        High values express positive sentiment, low negative ones negative sentiment.
+        Values close to 0 express neutral sentiment."""
+        pos = pnn[:, 0]
+        neg = pnn[:, 1]
+        # Transform range [0, 1] to [-1, 1]
+        # Ignore neutrality score as it's implicitly encoded as (1 - pos - neg)
+        polarities = pos - neg
+        return polarities
+
+    def predict_sentiment(self, texts: List[str])-> List[str]:
+        texts = [self.clean_text(text) for text in texts]
+        # Add special tokens takes care of adding [CLS], [SEP], <s>... tokens in the right way for each model.
+        input_ids = self.tokenizer.batch_encode_plus(texts, padding=True, add_special_tokens=True)
+        input_ids = torch.tensor(input_ids["input_ids"])
+
+        with torch.no_grad():
+            logits = self.model(input_ids)
+            probs = F.softmax(logits[0], dim=1)
+            # print(probs)
+        
+        polarities = self.probs2polarities(probs)
+        return polarities
+
+
+if __name__ == '__main__':
+    model = GSBertPolarityModel()
+    s = model.predict_sentiment(['Du hirnloser Vollidiot!', 'Ich mag dich sehr.'])
+    print(s)
