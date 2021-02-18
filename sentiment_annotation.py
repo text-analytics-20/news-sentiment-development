663bf04ddadf12ce0ef00ac0e4a5a614c200599f
silotin <simon.luedke@gmx.de>
2021-02-18 13:51:25 +0100

    Change labels to be coherent with training

--- sentiment_annotation.py
+++ sentiment_annotation.py
@@ -1,11 +1,12 @@
 def sentiment_annotation(data):
-#function to facilitate manual sentiment annotation of data
-#input: data as iterable: list of articles/ phrases/ words/ ...
-#output: dictionary with annotations in range (-1, 1)
+    # function to facilitate manual sentiment annotation of data
+    # input: data as iterable: list of articles/ phrases/ words/ ...
+    # output: dictionary with annotations in range (-1, 1)
 
     sentiment_dict = {}
-    translation = {'n': -1, 'p': 1, '': 0} #translates input to numerical values
-    print("sentiment input: p = positive, n = negative, press enter if neutral: ")
+    # translates input to numerical values
+    translation = {'n': 1, 'p': 0, '': 2}  
+    print("sentiment input: p = positive, n = negative, press enter if neutral:")
 	
     for phrase in data:
         print(phrase)
@@ -15,13 +16,14 @@ def sentiment_annotation(data):
         while sentiment not in ["n", "p", ""]:
             print("no valid input: p = positive, n = negative, press enter if neutral: ")
             print(phrase)
-            sentiment = input()   
-			
+            sentiment = input()		
         sentiment_dict[phrase] = translation[sentiment]
                            
     return sentiment_dict
 
-#toy example
-data = ["Schutzsuchender", "Wirtschaftsflüchtling", "Geflüchteter"]
-dict = sentiment_annotation(data)
-print(dict)
+
+if __name__ == "__main__":  
+    # toy example
+    data = ["Schutzsuchender", "Wirtschaftsflüchtling", "Geflüchteter"]
+    dict = sentiment_annotation(data)
+    print(dict)
