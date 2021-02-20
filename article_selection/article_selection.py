45741dceff76131a7dfcac411d6591d57fa6fed0
silotin <simon.luedke@gmx.de>
2021-02-20 18:10:39 +0100

    More logging of annotation file creation

--- article_selection/article_selection.py
+++ article_selection/article_selection.py
@@ -54,7 +54,7 @@ def is_topic_relevant(article, keywords: list = ['migra', 'flücht', 'asyl']):
 
 def write_relevant_content_to_file(file_list, relevant_articles_base, search_keywords, 
                                    new=False, training_size: int=1000, 
-                                   output_after=5000, seed=0):
+                                   output_after=5000, seed=0, annotation = True):
     if new:
         try:
             os.remove(relevant_articles_base+"_evaluation.json")
@@ -65,31 +65,33 @@ def write_relevant_content_to_file(file_list, relevant_articles_base, search_key
     print(f"Start selecting files. Number of files: {len(file_list)}")
     new_cont = {}
     for json_file in tqdm(file_list):
-       
         with open(json_file, "r") as jf:
             content = json.load(jf)
             if(is_topic_relevant(content)):
                 new_cont[json_file] = content
     
     print(f"Total number of relavant articles: {len(new_cont)}")
-    print(f"Size of training set: {training_size}")
-    print(f"    -> {training_size//3} articles per annotation file")
-    # seperate the files used for annotation and then training of the BERT Model
-    # 1. seed the random number generator for reproducable results
-    random.seed(seed)
+    if annotation:
+        print(f"Size of training set: {training_size}")
+        # seperate the files used for annotation and then training of the BERT Model
+        # 1. seed the random number generator for reproducable results
+        random.seed(seed)
 
-    # 2. sample the right number of keys
-    training_keys = random.sample(list(new_cont), training_size)
+        # 2. sample the right number of keys
+        training_keys = random.sample(list(new_cont), training_size)
 
-    # 3. create the train and evaluation dataset
-    train = {k: new_cont[k] for k in new_cont if k in training_keys}
-    eval = {k: new_cont[k] for k in new_cont if k not in training_keys}
+        # 3. create the train and evaluation dataset
+        train = {k: new_cont[k] for k in new_cont if k in training_keys}
+        eval = {k: new_cont[k] for k in new_cont if k not in training_keys}
 
-    # 4. split the training set in three for annotation
-    list_train = list(train)
-    ann_martin = {k: train[k] for k in list_train[: len(list_train)//3]}
-    ann_josephine = {k: train[k] for k in list_train[len(list_train)//3: 2*len(list_train)//3]}
-    ann_simon = {k: train[k] for k in list_train[2*len(list_train)//3: ]}
+        # 4. split the training set in three for annotation
+        list_train = list(train)
+        ann_martin = {k: train[k] for k in list_train[: len(list_train)//3]}
+        ann_josephine = {k: train[k] for k in list_train[len(list_train)//3: 2*len(list_train)//3]}
+        ann_simon = {k: train[k] for k in list_train[2*len(list_train)//3:]}
+        print(f"    -> {len(ann_martin)} articles per annotation file")
+    else:
+        eval = new_cont
 
     # save the data to files
     try:
@@ -98,31 +100,33 @@ def write_relevant_content_to_file(file_list, relevant_articles_base, search_key
             content_ra.update(eval)
             ra.seek(0)
             json.dump(content_ra, ra)
-        with open(relevant_articles_base+"_annotation_simon.json", "r+") as ra:
-            content_ra = json.load(ra)
-            content_ra.update(ann_simon)
-            ra.seek(0)
-            json.dump(content_ra, ra)
-        with open(relevant_articles_base+"_annotation_josephine.json", "r+") as ra:
-            content_ra = json.load(ra)
-            content_ra.update(ann_josephine)
-            ra.seek(0)
-            json.dump(content_ra, ra)
-        with open(relevant_articles_base+"_annotation_martin.json", "r+") as ra:
-            content_ra = json.load(ra)
-            content_ra.update(ann_martin)
-            ra.seek(0)
-            json.dump(content_ra, ra)
+        if annotation:
+            with open(relevant_articles_base+"_annotation_simon.json", "r+") as ra:
+                content_ra = json.load(ra)
+                content_ra.update(ann_simon)
+                ra.seek(0)
+                json.dump(content_ra, ra)
+            with open(relevant_articles_base+"_annotation_josephine.json", "r+") as ra:
+                content_ra = json.load(ra)
+                content_ra.update(ann_josephine)
+                ra.seek(0)
+                json.dump(content_ra, ra)
+            with open(relevant_articles_base+"_annotation_martin.json", "r+") as ra:
+                content_ra = json.load(ra)
+                content_ra.update(ann_martin)
+                ra.seek(0)
+                json.dump(content_ra, ra)
     except FileNotFoundError:
         # happens if new is enabled or function called the first time for a filepath
         with open(relevant_articles_base+"_evaluation.json", "w") as raf:
             json.dump(eval, raf)
-        with open(relevant_articles_base+"_annotation_simon.json", "w") as raf:
-            json.dump(ann_simon, raf)
-        with open(relevant_articles_base+"_annotation_josephine.json", "w") as raf:
-            json.dump(ann_josephine, raf)
-        with open(relevant_articles_base+"_annotation_martin.json", "w") as raf:
-            json.dump(ann_martin, raf)
+        if annotation:
+            with open(relevant_articles_base+"_annotation_simon.json", "w") as raf:
+                json.dump(ann_simon, raf)
+            with open(relevant_articles_base+"_annotation_josephine.json", "w") as raf:
+                json.dump(ann_josephine, raf)
+            with open(relevant_articles_base+"_annotation_martin.json", "w") as raf:
+                json.dump(ann_martin, raf)
     print(f"All files are written.")
 
 
