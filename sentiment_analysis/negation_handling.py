83555d7a5ecf2686832ae952b2079c5c74ad2670
Josephyne <75080965+Josephyne@users.noreply.github.com>
2021-02-22 13:55:13 +0100

    checking if whole phrase negated
    improved negation handling by checking if whole phrase is already negated

--- sentiment_analysis/negation_handling.py
+++ sentiment_analysis/negation_handling.py
@@ -2,30 +2,53 @@ import spacy
 from nltk.stem.snowball import SnowballStemmer
 
 stemmer = SnowballStemmer(language='german')
+nlp = spacy.load('de')
+neg_words = ['nicht', 'kein', 'nirgends', 'nirgendwo', 'niemand', 'niemals', 'nirgendwohin', 'nie']
 
+#More information: https://spacy.io/usage/linguistic-features
+
+
+def check_for_negated_sentence(sentence):
+    # checks if whole sentence is negated (= the main verb is negated); sentence already tokenized!
+    # returns sign (+1: not negated, -1: negated)
+    negation = 1
+    root = [token for token in sentence if token.dep_ == "ROOT"] #the main verb of the sentence
+    
+    if len(root) == 1:
+    	# uses the syntactic dependency between the words to detect if root is negated (token.children are the dependend words of token)
+    	children= [stemmer.stem(child.text) for child in root[0].children]
+    	if any(neg_word in children for neg_word in neg_words):
+    		negation = -1
+    		
+    return negation
+    		
 
 def check_for_negation(sentence, word):
     # checks if word in sentence is negated; BOTH (word and sentence) are already tokenized!
     # returns sign (+1: not negated, -1: negated)
 
     # TODO: handle case when word appears twice in the sentence
-    # TODO: Improve by checking negation of complete phrase (=negation of verb)
 
-    neg_words = ['nicht', 'kein', 'nirgends', 'nirgendwo', 'niemand', 'niemals', 'nirgendwohin', 'nie']
-    negation = 1
+    negation = check_for_negated_sentence(sentence)
+    	
     for token in sentence:
+
         if word == token:
-            # uses the syntactic dependency between the words to detect which word is negated (token.children, are the dependend words of token)
+
+            # uses the syntactic dependency between the words to detect which word is negated (token.children are the dependend words of token)
             children = [stemmer.stem(child.text) for child in token.children]
+            
             if any(neg_word in children for neg_word in neg_words):
-                negation = -1    
+                negation = negation* (-1)
+            	   
     return negation
 
 
 # example
 if __name__ == "__main__":
-    nlp = spacy.load('de')
-    doc1 = nlp('Der Apfel ist nicht gut.')
-    doc2 = nlp('Ich habe keine Angst.')
-    # print(check_for_negation(doc1, doc1[4]))
-    # print(check_for_negation(doc2, doc2[3]))
+    doc1 = nlp('Flüchtlinge sind böse')
+    doc2 = nlp('Flüchtlinge sind nicht böse')
+
+    print(check_for_negation(doc1, doc1[2]))
+    print(check_for_negation(doc2, doc2[3]))
+    
